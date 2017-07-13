99f905bfc3b75541a0273c6d9e9e1b79ce7714d9
Yongliang Yu <enigmaraul2@hotmail.com>
2017-07-12 23:23:34 -0700

    looks good

--- login.sh
+++ login.sh
@@ -4,4 +4,4 @@ echo "-->email server(after the @)?"
 read server
 echo "-->password?"
 read pw
-curl -H "X-Tidal-Token: _DSTon1kC8pABnTw" --data "username=$email%40$server&password=$pw&clientVersion=1.12.1&clientUniqueKey=" api.tidalhifi.com/v1/login/username
+curl --silent -H "X-Tidal-Token: _DSTon1kC8pABnTw" -H "User-Agent: TIDAL/362 CFNetwork/711.4.6 Darwin/14.0.0" --data "username=$email%40$server&password=$pw&clientVersion=1.12.1&clientUniqueKey=" api.tidalhifi.com/v1/login/username | jq -r ".sessionId"
